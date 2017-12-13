7820427b6d391c05f569e937518cd0965498d115
hamlet <m.abuganiev@student.inha.uz>
2017-12-13 19:30:44 +0500

    Server code being written

--- src/DMSystem_starter.c
+++ src/DMSystem_starter.c
@@ -1,14 +1,25 @@
 //
 // Created by hamlet on 12/13/17.
 //
+#pragma once
+
 #include <stdlib.h>
 #include <stdio.h>
 #include "../Structures/Structures.h"
-#include "../Structures/Location.h"
 
 char* initialPackageToSend = "Hello form server!\n";
 char* initialPackageToReceive;
 char* serverShutdownMessage = "SERVER IS GOING DOWN!";
+int client_is_active[MAX_CLIENTS + MAX_DRIVERS] = {};
+int clients_count = 0;
+struct Server server;
+
+//Will accept incoming connections
+void acceptConnections();
+
+//Start a connections session with client or driver
+void startSession(void*);
+
 
 int main(int argc, char** argv)
 {
@@ -16,51 +27,42 @@ int main(int argc, char** argv)
     // serverInit() is responsible for initialization of
     // (a.k.a memory allocation for) clients and drivers
 
-    struct Server server = serverInit();
+    server = serverInit();
     int addrlen = sizeof(struct sockaddr_in);
 
     printf("CREATING SERVER SOCKET .....\n");
 
-    Location a = locationInit();
-    a.latitude = 42.990967;
-    a.longitude =  -71.463767;
-
-    Location b = locationInit();
-    b.latitude = 41.990967;
-    b.longitude = -72.463767;
-
-    double test = a.calculateDistanceTo(&a, &b);
-    printf("%lf", test);
-
-
-// Creating socket file descriptor
-    /*
+    // Creating socket file descriptor
     if ((server.connection->socket = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
     printf("DEFINING SERVER SOCKET OPTIONS, FAMILY, ADDRESS & PORT .....\n");
-// Forcefully attaching socket to the port 8080
-    if (setsockopt(server.connection->socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+
+    if (setsockopt(server.connection->socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &server.opt, sizeof(server.opt)))
     {
         perror("setsockopt failed");
         exit(EXIT_FAILURE);
     }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+
+    //Initializing address family and port
+    server.connection->address.sin_family = AF_INET;
+    server.connection->address.sin_addr.s_addr = INADDR_ANY;
+    server.connection->address.sin_port = htons( DEFAULT_PORT );
     printf("BINDING SERVER SOCKET TO PORT 8080 .....\n");
-// Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server.connection->socket, (struct sockaddr *)&server.connection->address, sizeof(server.connection->address))<0)
     {
         perror("socket bind failed");
         exit(EXIT_FAILURE);
     }
 
-    //Listen
+    //Listening...
     printf("SERVER LISTENING ON PORT 8080 FOR NEW CONNECTION.....\n");
-    if (listen(server_fd, 7) < 0)
+    //We wait for at most MAX_DRIVERS + MAX_CLIENTS connections,
+    if (listen(server.connection->socket, MAX_DRIVERS + MAX_CLIENTS) < 0)
     {
         perror("socket listen failed ");
         exit(EXIT_FAILURE);
@@ -76,6 +78,40 @@ int main(int argc, char** argv)
 
 
 
-    */
+
     return 0;
+}
+
+
+void acceptConnections() {
+    while (clients_count <= MAX_DRIVERS + MAX_CLIENTS)
+    {
+        int i = 0;
+
+        //this loop finds a not vacant place in activity array
+        while (client_is_active[i++] != 0);
+
+        //and reserve found place for a new client to be connected
+        client_is_active[i] = 1;
+
+        //Wait for a connection and accept it
+        if((server.clients[i]->connection->socket =
+                    accept(server.connection->socket,
+                           (struct sockaddr*)&server.connection->address,
+                           (socklen_t*)&server.connection->address)) < 0)
+        {
+            perror("SOCKET ACCEPT FAILED");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("SERVER ACCEPTED NEW CONNECTION FROM A CLIENT ON PORT %d .....\n", DEFAULT_PORT);
+
+        //some volatile variables must be declared here
+//        clients[i]->client_id = i;
+//        clients[i]->isUp = 1;
+//        clients_count++;
+
+
+
+    }
 }
