db1c3d3057fb9bade89e1151bd5bab450b061db5
hamlet <m.abuganiev@student.inha.uz>
2017-12-15 16:01:19 +0500

    Server code

--- src/Server.h
+++ src/Server.h
@@ -21,7 +21,7 @@ int drivers_count = 0;
 
 
 //Will accept incoming connections
-void acceptConnections();
+void acceptConnections(Server server);
 
 /*
  * Start a connections session with client or driver
@@ -48,39 +48,56 @@ void driverConnectionThread(void*);
 
 
 
-void acceptConnections() {
+void acceptConnections(Server server) {
     while (clients_count <= MAX_DRIVERS + MAX_CLIENTS)
     {
         int i = 0;
-
+        int j = 0;
+        int sock;
         //this loop finds a not vacant place in activity array
-        while (client_is_active[i++] != 0);
-
-        //and reserve found place for a new client to be connected
-        client_is_active[i] = 1;
+        while (client_is_active[0][i++] != 0);
+        while (driver_is_active[0][j++] != 0);
 
         //Wait for a connection and accept it
-        if((server.clients[i]->connection->socket =
-                    accept(server.connection->socket,
-                           (struct sockaddr*)&server.connection->address,
-                           (socklen_t*)&server.connection->address)) < 0)
+        if((sock = accept(server.connection->socket,
+                          (struct sockaddr*)&server.connection->address,
+                          (socklen_t*)&server.connection->address)) < 0)
         {
             perror("SOCKET ACCEPT FAILED");
             exit(EXIT_FAILURE);
         }
 
-        printf("SERVER ACCEPTED NEW CONNECTION FROM A CLIENT ON PORT %d .....\n", DEFAULT_PORT);
+        char* initBuff[MAX_BUFFER];
 
-        //some volatile variables must be declared here
-//        clients[i]->client_id = i;
-//        clients[i]->isUp = 1;
-//        clients_count++;
+        if(read(sock, initBuff, MAX_BUFFER) < 0)
+        {
+            perror("ERROR ON READ FROM SOCKET");
+            exit(EXIT_FAILURE);
+        }
 
-        //if(isClient)
-        startSession(server.clients[i], 0);
-        //else
-        startSession(server.drivers[i], 1);
+        //TODO parse initBuff from JSON to obj or string
 
+
+        if(true)
+        {
+            client_is_active[0][i] = 1;
+            //TODO initialize client id, get it from JSON object
+            client_is_active[1][i] = server.clients[i].id;
+            server.clients[i].isUp = 1;
+            server.clients[i]->connection->socket = sock;
+            printf("SERVER ACCEPTED NEW CONNECTION FROM A CLIENT ON PORT %d .....\n", DEFAULT_PORT);
+            startSession(server.clients[i], 0);
+        }
+        else
+        {
+            driver_is_active[0][j] = 1;
+            //TODO initialize client id, get it from JSON object
+            driver_is_active[1][j] = server.drivers[j].id;
+            server.drivers[i].isUp = 1;
+            server.drivers[j]->connection->socket = sock;
+            printf("SERVER ACCEPTED NEW CONNECTION FROM A CLIENT ON PORT %d .....\n", DEFAULT_PORT);
+            startSession(server.drivers[i], 1);
+        }
 
     }
 }
@@ -91,8 +108,12 @@ void startSession(void* obj, int obj_type) {
     switch (obj_type) {
         case CLIENT_OBJ:
 
+            Client* client = obj;
+
             break;
         case DRIVER_OBJ:
+
+            Driver* driver = obj;
 
             break;
 
