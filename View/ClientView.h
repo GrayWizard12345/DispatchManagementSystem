fa9bbd11b3c6638ceb27a91727c4aa858566ebec
FunnyKorean <lkim2695@gmail.com>
2017-12-17 15:30:53 +0500

    Views are updated

--- View/ClientView.h
+++ View/ClientView.h
@@ -3,3 +3,89 @@
 //
 
 #pragma once
+
+#include <stdio.h>
+
+void clientMainMenuView () {
+    int choice = 0;
+
+    printf("\t\tMain Menu\n");
+    printf("\t1.Make order\n");
+    printf("\t2.Cancel order\n");
+
+    while (choice != 1 && choice != 2) {
+        scanf("%d", &choice);
+    }
+
+    switch (choice) {
+
+        case 1:
+            break;
+        case 2:
+            break;
+        default:
+            break;
+    }
+}
+
+void clientMakeOrderView () {
+    clientEnterClientInfoView();
+    clientEnterLocationView("source");
+    clientEnterLocationView("destination");
+
+}
+
+void clientCancelOrderView() {
+
+
+}
+
+void clientEnterClientInfoView() {
+    char* name;
+    char* phoneNumber;
+    printf("Enter your name\n");
+    scanf("%s", name);
+    printf("Enter your phone number\n");
+    scanf("%s", phoneNumber);
+
+}
+
+void clientEnterLocationView(char *option ) {
+    double latitude;
+    double longitude;
+    if (option == "source") {
+        printf("Enter your coordinates latitude and longitude of your source\n");
+        scanf("%lf", &latitude);
+        scanf("%lf", &longitude);
+    }else {
+        printf("Enter your coordinates latitude and longitude of destination\n");
+        scanf("%lf", &latitude);
+        scanf("%lf", &longitude);
+    }
+}
+
+void clientOrderAccepted() {
+    int choice = 0;
+
+    printf("\t\tYour order is accepted\n");
+    printf("Driver info:");
+    printf("Car number: %s");
+    printf("Car color: %s");
+    printf("Car model: %s");
+    printf("---------------------------");
+    printf("\n1.Cancel order\n");
+
+    while (choice != 1) {
+        scanf("%d", &choice);
+    }
+
+    switch (choice) {
+
+        case 1:
+            break;
+        default:
+            break;
+    }
+
+
+}
--- View/DriverView.h
+++ View/DriverView.h
@@ -3,3 +3,64 @@
 //
 
 #pragma once
+#include <stdio.h>
+
+void driverLoginView () {
+    int id;
+    char* password;
+
+    printf("\t\tLogin");
+    printf("Enter your ID: ");
+    scanf("%d",&id);
+    printf("Enter your password");
+    scanf("%s", password);
+}
+
+void driverMainMenuView (double latitude, double longitude) {
+    printf("\t\t Waiting for the order\n");
+    printf("-------------------------------\n");
+    printf("\n\n\tLocation: %lf,%lf", latitude, longitude);
+
+}
+
+void driverHasOrderView (double latitude, double longitude) {
+    printf("\t\tOrder information: \n");
+    printf("Client's name: %s\n");
+    printf("\n\n\tLocation to pick up: %lf,%lf\n", latitude, longitude);
+    printf("--------------------------\n");
+    printf("1.Arrived to pick up\n");
+
+    while (choice != 1) {
+        scanf("%d", &choice);
+    }
+
+    switch (choice) {
+
+        case 1:
+            break;
+        default:
+            break;
+    }
+}
+
+void driverWaitingClientView () {
+    printf("Waiting for the client\n");
+}
+
+void driverDriveView (double latitude, double longitude) {
+    printf("Driving\n");
+    printf("Destination: %lf, %lf\n", latitude,longitude);
+    printf("------------------------------\n");
+    printf("1.Client is delivered\n");
+    while (choice != 1) {
+        scanf("%d", &choice);
+    }
+
+    switch (choice) {
+
+        case 1:
+            break;
+        default:
+            break;
+    }
+}
