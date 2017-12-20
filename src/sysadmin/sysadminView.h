a1e79f40c7209390e7eb9c5aaaa7bc311bdd50a2
Abdulbosid <abdulbosid.kh@gmail.com>
2017-12-21 01:49:45 +0500

    its now working ura - getAllDrivers - updated)

--- src/sysadmin/sysadminView.h
+++ src/sysadmin/sysadminView.h
@@ -5,6 +5,10 @@
 #ifndef SYSADMINVIEW_H
 #define SYSADMINVIEW_H
 
+#define OK       0
+#define NO_INPUT 1
+#define TOO_LONG 2
+
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -15,6 +19,7 @@
 void addNewDriverView();
 void editDriverView();
 void allDriversView();
+static int myGetLine (char *prmpt, char *buff, size_t sz);
 
 void sysadminView()
 {
@@ -70,23 +75,34 @@ void addNewDriverView()
     /*Reading password*/
     char *password;
     char password2[INPUT_STRING_LENGTH] = {'\0'};
-    Vehicle vehicle;
 
-    char number[8]; //TODO: size of the variable is not defined in the global
-    char model[MAX_BUFFER];
-    char color[MAX_BUFFER];
+    size_t bufferSize = MAX_BUFFER;
+    size_t bufferSizeForNumber = 8; //TODO: size of the variable is not defined in the global
+
+    char *number = (char *)malloc(bufferSizeForNumber * sizeof(char));
+    char *model = (char *)malloc(bufferSize * sizeof(char));
+    char *color = (char *)malloc(bufferSize * sizeof(char));
 
     password = getpass("  *Please enter the new password: ");
     strcpy(password2, password);
     password = getpass("  *Please confirm the new password: ");
 
-    printf("   Please enter the new Driver's vehicle number:\n");
-    getline(number, (sizeof(char)*8), stdin);
-    printf("   Please enter the new Driver's vehicle model:\n");
-    getline(model, (sizeof(char)*MAX_BUFFER), stdin);
-    printf("   Please enter the new Driver's vehicle color:\n");
-    getline(color, (sizeof(char)*MAX_BUFFER), stdin);
+    myGetLine("Please enter the new Driver's vehicle number: ", number, bufferSizeForNumber);
+    myGetLine("Please enter the new Driver's vehicle model: ", model, bufferSize);
+    myGetLine("Please enter the new Driver's vehicle color: ", color, bufferSize);
 
+
+    Vehicle vehicle;
+    strcpy(vehicle.number, number);
+    strcpy(vehicle.model, model);
+    strcpy(vehicle.color, color);
+//    printf("%d - %s", numberLen, number);
+//    if (number[numberLen-1] == '\n')
+//        number[numberLen-1] = '\0';
+//    if (model[modelLen-1] == '\n')
+//        model[modelLen-1] = '\0';
+//    if (color[colorLen-1] == '\n')
+//        color[colorLen-1] = '\0';
 
     if (strcmp(password, password2) == 0)
     {
@@ -117,10 +133,34 @@ void allDriversView()
     printf("-    Drivers registered in the DMSystem       -\n");
     DriverArray driverArray = getAllDrivers();
     for (int i = 0; i < driverArray.used; ++i) {
-        printf("%d.Driver id: %d\n  Vehicle number:%s\n  Vehical model: %s\n  Vehical color: %s\n", i, driverArray.array[i].id, driverArray.array[i].vehicle.number, driverArray.array[i].vehicle.model, driverArray.array[i].vehicle.color);
+        printf("%d.Driver id: %d\n  Vehicle password:%s\n  Vehicle number:%s\n  Vehical model: %s\n  Vehical color: %s\n", i, driverArray.array[i].id, driverArray.array[i].password, driverArray.array[i].vehicle.number, driverArray.array[i].vehicle.model, driverArray.array[i].vehicle.color);
     }
     printf("-----------------------------------------------\n");
 }
 
+
+static int myGetLine (char *prmpt, char *buff, size_t sz) {
+    int ch, extra;
+    // Get line with buffer overrun protection.
+    if (prmpt != NULL) {
+        printf ("%s", prmpt);
+        fflush (stdout);
+    }
+    if (fgets (buff, sz, stdin) == NULL)
+        return NO_INPUT;
+
+    // If it was too long, there'll be no newline. In that case, we flush
+    // to end of line so that excess doesn't affect the next call.
+    if (buff[strlen(buff)-1] != '\n') {
+        extra = 0;
+        while (((ch = getchar()) != '\n') && (ch != EOF))
+            extra = 1;
+        return (extra == 1) ? TOO_LONG : OK;
+    }
+
+    // Otherwise remove newline and give string back to caller.
+    buff[strlen(buff)-1] = '\0';
+    return OK;
+}
 
 #endif //SYSADMINVIEW_H
